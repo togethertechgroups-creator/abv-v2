f54dd7d9a51239d99173cda5b59b164251acbb7f
Aarsha <aarshak2004@gmail.com>
2026-03-15 13:17:00 +0530

    perf: Phase 2 optimizations (accessibility, React.memo, PurgeCSS)

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -117,4 +117,4 @@ function Footer() {
   );
 }
 
-export default Footer;
+export default React.memo(Footer);
--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -125,4 +125,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default React.memo(Header);
